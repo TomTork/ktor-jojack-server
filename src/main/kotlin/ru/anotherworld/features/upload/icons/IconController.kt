84c6bda169c61d5a2c43342951fb76eb1d4ebb77
TomTork <52648973+TomTork@users.noreply.github.com>
2024-03-06 19:35:28 +0300

    ава

--- src/main/kotlin/ru/anotherworld/features/upload/icons/IconController.kt
+++ src/main/kotlin/ru/anotherworld/features/upload/icons/IconController.kt
@@ -5,8 +5,7 @@ import io.ktor.server.request.*
 import java.io.File
 
 class IconController {
-    suspend fun getIcon(call: ApplicationCall) : File?{
-        val response = call.receive<IconRemote>()
-        return File("C:/Users/Rescue/Documents/ktor-jojack-server/src/main/kotlin/ru/anotherworld/files/images/icons/${response.login}.png") ?: null
+    suspend fun getIcon(call: ApplicationCall, login: String) : File?{
+        return File("C:/Users/Rescue/Documents/ktor-jojack-server/src/main/kotlin/ru/anotherworld/files/images/icons/${login}.png") ?: null
     }
 }
--- src/main/kotlin/ru/anotherworld/features/upload/icons/IconRouting.kt
+++ src/main/kotlin/ru/anotherworld/features/upload/icons/IconRouting.kt
@@ -1,5 +1,6 @@
 package ru.anotherworld.features.upload.icons
 
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -13,17 +14,33 @@ import java.io.File
 fun Application.configureIconRouting() {
     routing {
         get("/icon"){
-            val iconController = IconController()
-            val data = iconController.getIcon(call)
-            if(data != null) call.respondFile(data)
-            else call.respondText("null")
+            val login = call.parameters["login4"]
+            if (login != null){
+                val iconController = IconController()
+                val data = iconController.getIcon(call, login)
+                if(data != null) call.respondFile(data)
+            }
         }
         post("/set-icon") {
             val response = call.parameters["login3"]
             val token = call.parameters["token3"]
             if(token != null && TokenDatabase2().getLoginByToken(token) == response){
                 val file = File("C:/Users/Rescue/Documents/ktor-jojack-server/src/main/kotlin/ru/anotherworld/files/images/icons/${response}.png")
-                call.receiveChannel().copyAndClose(file.writeChannel())
+                if (file.exists()) file.delete()
+
+                val multipart = call.receiveMultipart()
+                multipart.forEachPart { part ->
+                    if(part is PartData.FileItem) {
+                        part.streamProvider().use { its ->
+                            file.outputStream().buffered().use {
+                                its.copyTo(it)
+                            }
+                        }
+                    }
+                    part.dispose()
+                }
+//                val file = File("C:/Users/Rescue/Documents/ktor-jojack-server/src/main/kotlin/ru/anotherworld/files/images/icons/${response}.png")
+//                call.receiveChannel().copyAndClose(file.writeChannel())
             }
         }
     }
